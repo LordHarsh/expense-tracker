15bf5d78d4f1f2fdb09c0674f3f3fbca575ea52a
Lord Harsh <harshbanka321@gmail.com>
2023-04-26 00:42:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,83 @@
+
+const express = require('express')
+const mongodb = require('mongodb')
+const dotenv = require('dotenv')
+const ejs = require('ejs')
+const { body, validationResult } = require('express-validator');
+
+dotenv.config()
+
+const app = express()
+
+// set up EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const client = new mongodb.MongoClient(mongoUri || 'mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect((err) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log('Connected to MongoDB');
+
+    // start the server
+    app.listen(port, () => {
+        console.log(`Server started on port ${port}`);
+    });
+});
+
+// define routes
+app.get('/', (req, res) => {
+    const expenses = client.db().collection('expenses').find().toArray((err, expenses) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            console.log('Working')
+            res.render('index', { expenses });
+        }
+    });
+});
+
+app.post('/expenses', [
+    body('name').trim().isLength({ min: 1 }).escape().withMessage('Name is required'),
+    body('amount').trim().isLength({ min: 1 }).withMessage('Amount is required').toFloat(),
+    body('date').trim().isLength({ min: 1 }).withMessage('Date is required').toDate(),
+    body('category').trim().isLength({ min: 1 }).escape().withMessage('Category is required'),
+  ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const expenses = client.db().collection('expenses').find().toArray((err, expenses) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          res.render('index', { expenses, errors: errors.array() });
+        }
+      });
+    } else {
+      const expense = {
+        name: req.body.name,
+        amount: req.body.amount,
+        date: req.body.date,
+        category: req.body.category,
+      };
+      client.db().collection('expenses').insertOne(expense, (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          res.redirect('/');
+        }
+      });
+    }
+  });
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log('Server is listening on port 5000...');
+})
---
+++ server.js
@@ -0,0 +1,34 @@
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri = 'mongodb://localhost:27017';
+
+const client = new MongoClient(uri);
+client.connect((err) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log('Connected to MongoDB');
+
+    // start the server
+    app.listen(port, () => {
+        console.log(`Server started on port ${port}`);
+    });
+});
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
